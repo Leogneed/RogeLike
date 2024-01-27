ea95e160c20237916afb2c79604b377aa87a21c4
Simakov Viacheslav <106369371+Leogneed@users.noreply.github.com>
2024-01-28 01:22:55 +1000

    Врагов добавил

--- RougeLike/Program.cs
+++ RougeLike/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 class Program
 {
@@ -7,10 +8,24 @@ class Program
     static int playerY;
     static int exitX = 18;
     static int exitY = 18;
+    static List<Enemy> enemies = new List<Enemy>();
+    static Random random = new Random();
 
+    class Enemy
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Enemy(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
     static void Main()
     {
         GenerateMaze();
+        GenerateEnemies();
         playerX = 0;
         playerY = 0;
 
@@ -20,6 +35,8 @@ class Program
         {
             Console.Clear();
             DrawMaze();
+            DrawEnemies();
+            MoveEnemies();
             keyInfo = Console.ReadKey();
             HandleKeyPress(keyInfo);
         } while (playerX != exitX || playerY != exitY);
@@ -55,6 +72,30 @@ class Program
 
         // Очистка позиции игрока
         maze[0, 0] = '.';
+    }
+
+    static void GenerateEnemies()
+    {
+        int numberOfEnemies = random.Next(3, 7);
+
+        for (int i = 0; i < numberOfEnemies; i++)
+        {
+            int enemyX;
+            int enemyY;
+
+            do
+            {
+                enemyX = random.Next(2, 18);
+                enemyY = random.Next(2, 18);
+            } while (maze[enemyX, enemyY] != '.' || IsTooCloseToPlayerOrExit(enemyX, enemyY));
+
+            enemies.Add(new Enemy(enemyX, enemyY));
+        }
+    }
+
+    static bool IsTooCloseToPlayerOrExit(int x, int y)
+    {
+        return Math.Abs(x - playerX) < 5 || Math.Abs(y - playerY) < 5 || Math.Abs(x - exitX) < 5 || Math.Abs(y - exitY) < 5;
     }
 
     static void RecursiveBacktracker(int x, int y)
@@ -100,12 +141,25 @@ class Program
                 {
                     Console.Write("P ");
                 }
+                else if (x == exitX && y == exitY)
+                {
+                    Console.Write(". ");
+                }
                 else
                 {
                     Console.Write(maze[x, y] + " ");
                 }
             }
             Console.WriteLine();
+        }
+    }
+
+    static void DrawEnemies()
+    {
+        foreach (var enemy in enemies)
+        {
+            Console.SetCursorPosition(enemy.X * 2, enemy.Y);
+            Console.Write("E ");
         }
     }
 
@@ -138,6 +192,22 @@ class Program
         {
             playerX = newPlayerX;
             playerY = newPlayerY;
+        }
+    }
+    static void MoveEnemies()
+    {
+        foreach (var enemy in enemies)
+        {
+            int direction = random.Next(4);
+            int newEnemyX = enemy.X + (direction == 0 ? 1 : direction == 1 ? -1 : 0);
+            int newEnemyY = enemy.Y + (direction == 2 ?
+            1 : direction == 3 ? -1 : 0);
+
+            if (newEnemyX >= 0 && newEnemyX < 20 && newEnemyY >= 0 && newEnemyY < 20 && maze[newEnemyX, newEnemyY] != '█' && !IsTooCloseToPlayerOrExit(newEnemyX, newEnemyY))
+            {
+                enemy.X = newEnemyX;
+                enemy.Y = newEnemyY;
+            }
         }
     }
 }
